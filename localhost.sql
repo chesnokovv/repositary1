82f4cf7a9795c8bcf0addf2be37b4cb33a7323fe
chesnokovv <144803571+chesnokovv@users.noreply.github.com>
2023-09-12 16:18:27 +0300

    Add files via upload

---
+++ localhost.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost:3306
+-- Время создания: Сен 12 2023 г., 13:14
+-- Версия сервера: 5.6.34-log
+-- Версия PHP: 7.1.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `test`
+--
+CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `test`;
+--
+-- База данных: `черепашки-ниндзя`
+--
+CREATE DATABASE IF NOT EXISTS `черепашки-ниндзя` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `черепашки-ниндзя`;
+--
+-- База данных: `черепашки_ниндзя`
+--
+CREATE DATABASE IF NOT EXISTS `черепашки_ниндзя` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `черепашки_ниндзя`;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
